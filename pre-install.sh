19ad114b38cf861150372c65251167e21a4e02c1
KeeganForelight <Keegan@forelight.com>
2022-03-07 17:00:19 -0500

    tmux config added

--- pre-install.sh
+++ pre-install.sh
@@ -8,7 +8,7 @@ else
 	echo "Consider updating oh-my-zsh using: omz update"
 fi
 
-if [ -n "$ZSH_NAME" ]; then
+if [ -n $ZSH_NAME ]; then
 	echo "Changing default shell to zsh"
 	chsh -s /bin/zsh 
 else
