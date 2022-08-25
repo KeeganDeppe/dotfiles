302715395120c4e4ac27a52a4d2f7f361b8ba04e
Keegan <Keegan@forelight.com>
2022-08-25 17:56:11 -0400

    fixing weather script to remove api key need

---
+++ bin/weather/symbols.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+# source symbol based on $WEATHER_CODE and $TOD
+# WeatherCodesDay := map[string]rune{
+# "Unkown":              '\ue374',
+# "Cloudy":              '\ue302',
+# "Fog":                 '\ue303',
+# "HeavyRain":           '\ue318',
+# "HeavyShowers":        '\ue318',
+# "HeavySnow":           '\ue31a',
+# "HeavySnowShowers":    '\ue31a',
+# "LightRain":           '\ue308',
+# "LightShowers":        '\ue308',
+# "LightSleet":          '\ue3aa',
+# "LightSleetShowers":   '\ue3aa',
+# "LightSnow":           '\ue30a',
+# "LightSnowShowers":    '\ue30a',
+# "PartlyCloudy":        '\ue37b',
+# "Sunny":               '\ue30d',
+# "ThunderyHeavyRain":   '\ue31d',
+# "ThunderyShowers":     '\ue31d',
+# "ThunderySnowShowers": '\ue365',
+# "VeryCloudy":          '\ue312',
+# }
+
+# WeatherCodesNight := map[string]rune{
+# "Unkown":              '\ue374',
+# "Cloudy":              '\ue37e',
+# "Fog":                 '\ue346',
+# "HeavyRain":           '\ue318',
+# "HeavyShowers":        '\ue318',
+# "HeavySnow":           '\ue31a',
+# "HeavySnowShowers":    '\ue31a',
+# "LightRain":           '\ue325',
+# "LightShowers":        '\ue325',
+# "LightSleet":          '\ue3ac',
+# "LightSleetShowers":   '\ue3ac',
+# "LightSnow":           '\ue361',
+# "LightSnowShowers":    '\ue361',
+# "PartlyCloudy":        '\ue379',
+# "Sunny":               '\ue32b',
+# "ThunderyHeavyRain":   '\ue31d',
+# "ThunderyShowers":     '\ue31d',
+# "ThunderySnowShowers": '\ue366',
+# "VeryCloudy":          '\ue312',
+# }
