0b9c8429b83b79c5fa5a6c6dc3442d2433a561b5
spinach <spinach@Thinkpad.com>
2022-08-25 23:16:39 -0400

    made a basic bb script output with a 1 minute rotating display and unicode characters

--- bin/bluebikes/get_status.sh
+++ bin/bluebikes/get_status.sh
@@ -5,9 +5,22 @@ timeout=120 # in seconds
 refresh_rate=10 # display each station for 10 seconds
 WORKINGDIR="$HOME/.dotfiles/bin/bluebikes"
 
+if [[ ! -d "$WORKINGDIR/data" ]] ; then
+    mkdir -p "$WORKINGDIR/data"
+fi
+
 # getting time and existing information
 TIME=$(date +%s)
-OLD_EXPIRATION=$(ls "$WORKINGDIR/data/" | cut -d '.' -f 1 2>/dev/null) # removes .json extension
+OLD_EXPIRATION=$(ls "$WORKINGDIR/data" | cut -d '.' -f 1 2>/dev/null) # removes .json extension
+
+get_station_info() {
+    # cleans data folder and leaves only most recent copy
+    rm "$WORKINGDIR/data/"*.json 2>/dev/null
+    BB_STATUS=$(curl --silent -fL https://gbfs.bluebikes.com/gbfs/en/station_status.json)
+    EXPIRATION=$(($TIME + 120)) # sets expiration to be in 120 seconds
+    echo "$BB_STATUS" > "$WORKINGDIR/data/$EXPIRATION.json"
+    #echo $EXPIRATION
+}
 
 update_stations() {
     # checks to see if we are out of date and sets $output to be station info
@@ -17,26 +30,27 @@ update_stations() {
     fi
 
     STATIONS=$(cat "$WORKINGDIR/conf.json" | gojq '.stations') # stations we care about from config file
-    STATION_INFO=$(cat "$WORKINGDIR/"data/*.json) # all station information
+    STATION_INFO=$(cat "$WORKINGDIR/"data/*.json 2>/dev/null) # all station information
 
     # all associated data for the specific ids
     info=$(echo "$STATION_INFO" | gojq --argjson js "$STATIONS" '[.data.stations[] | select( .station_id as $id | $js.[].station_id | tostring | . == $id )]')
     # only available bikes/docks for the required ids
-    output=$(echo "$info" | gojq --argjson names "$STATIONS" '.[] | .station_id as $id | [{ id: $id, bikes: .num_bikes_available, docks: .num_docks_available, station: $names.[] | select( .station_id | tostring | . == $id ) | .name }]')
+    output=$(echo "$info" | gojq --argjson names "$STATIONS" '[ .[] | .station_id as $id | { bikes: .num_bikes_available, docks: .num_docks_available, station: $names.[] | select( .station_id | tostring | . == $id ) | .name }]')
     
 #    echo "$output"
 }
 
-get_station_info() {
-    # cleans data folder and leaves only most recent copy
-    rm "$WORKINGDIR/data/"*.json 2>/dev/null
-    BB_STATUS=$(curl --silent -fL https://gbfs.bluebikes.com/gbfs/en/station_status.json)
-    EXPIRATION=$(($TIME + 120)) # sets expiration to be in 120 seconds
-    echo $EXPIRATION
-}
 
 output_status() {
     # reads $OUTPUT and the Index value
+    minute=$(date +%M | tr -d '0')
+    tot=$(echo "$STATIONS" | gojq '. | length') # display one per minute
+    index=$(( $minute % $tot ))
+    bikes=$(echo "$output" | gojq --arg indx $index '.[$indx | tonumber ].bikes')
+    docks=$(echo "$output" | gojq --arg indx $index '.[$indx | tonumber ].docks')
+    station=$(echo "$output" | gojq --arg indx $index '.[$indx | tonumber ].station' | tr -d '"')
+    printf '%d   %d   %s' "$docks" "$bikes" "$station"
+}
 
 update_stations # sets $output to be all the stations we are interested in
 output_status # echos correct station info based on index and time
--- bin/weather/weather.sh
+++ bin/weather/weather.sh
@@ -11,9 +11,9 @@ if [[ $CUR_TIMESTAMP -gt $EXPIRATION  || -z $EXPIRATION ]] ; then
     WEATHER=$(curl --silent http://api.openweathermap.org/data/2.5/weather\?lat="$LAT"\&lon="$LON"\&appid="$API_KEY"\&units=imperial)
 
     echo $WEATHER
-    TEMP=$(echo $WEATHER | jq -r '.main.temp')
-    HUMIDITY=$(echo $WEATHER | jq -r '.main.humidity')
-    ICON=$(echo $WEATHER | jq -r '.weather[0].icon')
+    TEMP=$(echo $WEATHER | gojq -r '.main.temp')
+    HUMIDITY=$(echo $WEATHER | gojq -r '.main.humidity')
+    ICON=$(echo $WEATHER | gojq -r '.weather[0].icon')
     EXPIRATION=$(($CUR_TIMESTAMP+30))
 
     printf 'TEMP=%s\nHUMIDITY=%s\nICON=%s\nEXPIRATION=%s\n' "$TEMP" "$HUMIDITY" "$ICON" "$EXPIRATION" >"$HOME/.dotfiles/bin/weather/.env.weather"
